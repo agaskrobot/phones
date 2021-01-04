7f0330ed645d084d823fa8ee686fce6fe9f394a9
Aga <agnieszka.skrobot1@gmail.com>
2021-01-04 10:39:09 +0100

    added .env file

--- src/api/phone.js
+++ src/api/phone.js
@@ -1,16 +1,18 @@
 import axios from 'axios/index';
 
+const baseUrl = process.env.REACT_APP_API_URL + process.env.REACT_APP_API_URL_PREFIX + '/phones';
+
 // Get all phones
-export const getPhoneList = () => axios.get('http://localhost:8080/api/v1/phones');
+export const getPhoneList = () => axios.get(baseUrl);
 
 // Get phone
-export const getPhone = (phoneId) => axios.get(`http://localhost:8080/api/v1/phones/${phoneId}`);
+export const getPhone = (phoneId) => axios.get(`${baseUrl}/${phoneId}`);
 
 // Delete phone
-export const deletePhone = (phoneId) => axios.delete(`http://localhost:8080/api/v1/phones/${phoneId}`);
+export const deletePhone = (phoneId) => axios.delete(`${baseUrl}/${phoneId}`);
 
 // Update existing phone
-export const updatePhone = (phoneId, data) => axios.put(`http://localhost:8080/api/v1/phones/${phoneId}`, data);
+export const updatePhone = (phoneId, data) => axios.put(`${baseUrl}/${phoneId}`, data);
 
 // Create new phone
-export const addPhone = (data) => axios.post('http://localhost:8080/api/v1/phones/', { name: data });
+export const addPhone = (data) => axios.post(baseUrl, { name: data });
